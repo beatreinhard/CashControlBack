1eb46a8d225afcb1c0e200d7d8b6540bd3c1bdf6
Beat Reinhard <beatreinhard@bluewin.ch>
2024-02-06 22:41:15 +0100

    Spotless-Dependency entfernt und Import-Fehler korrigiert

--- src/main/java/ch/reinhard/cashcontrol/modules/steuern/application/service/BerufServiceImpl.java
+++ src/main/java/ch/reinhard/cashcontrol/modules/steuern/application/service/BerufServiceImpl.java
@@ -1,14 +1,8 @@
 package ch.reinhard.cashcontrol.modules.steuern.application.service;
 
 import ch.reinhard.cashcontrol.modules.steuern.api.BerufService;
-import ch.reinhard.cashcontrol.modules.steuern.application.domain.Beruf;
-
-import java.util.List;
 
 public class BerufServiceImpl implements BerufService {
 
-    @Override
-    public List<Beruf> getAll() {
-        return List.of();
-    }
+
 }
