5cbc5c782e19b1c469e8ecf2667bbed1c7d9af27
Beat Reinhard <beatreinhard@bluewin.ch>
2025-03-31 23:03:49 +0200

    Update JDK 21 und Spring-Boot 3.4.3

--- src/test/java/ch/reinhard/cashcontrol/modules/steuern/controller/BerufControllerTest.java
+++ src/test/java/ch/reinhard/cashcontrol/modules/steuern/controller/BerufControllerTest.java
@@ -7,8 +7,8 @@ import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -28,7 +28,7 @@ public class BerufControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private BerufService service;
 
     @Test
