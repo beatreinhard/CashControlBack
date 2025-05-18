c2e95204061b4ab2beaba49efff2cafeb7b0b126
Beat Reinhard <beatreinhard@bluewin.ch>
2025-05-18 17:13:21 +0200

    side-nav hinzugefügt

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListItem, MatNavList} from '@angular/material/list';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatToolbarModule, MatIconModule, MatListItem, MatNavList],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
