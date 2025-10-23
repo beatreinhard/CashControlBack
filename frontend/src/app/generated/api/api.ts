93cc170eb200ff088d81e204a30005793f97a9e2
Beat Reinhard <beatreinhard@bluewin.ch>
2025-10-23 17:07:04 +0200

    frontend/generated gelöscht

--- frontend/src/app/generated/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './ausgabeController.service';
-import {AusgabeControllerApi} from './ausgabeController.service';
-
-export * from './ausgabeController.serviceInterface';
-export const APIS = [AusgabeControllerApi];
--- frontend/src/app/generated/model/models.ts
+++
@@ -1,2 +0,0 @@
-export * from './ausgabeDto';
-export * from './ausgabeKategorieDto';
