fe58393a71f435c2f539d0db8def4c458544e70e
Beat Reinhard <beatreinhard@bluewin.ch>
2026-03-26 11:25:24 +0100

    update thymeleaf version, fragment und pdf-variables hinzugefügt

--- backend/src/main/java/ch/reinhard/cashcontrol/modules/reporting/ThymeleafTemplate.java
+++ backend/src/main/java/ch/reinhard/cashcontrol/modules/reporting/ThymeleafTemplate.java
@@ -3,7 +3,7 @@ package ch.reinhard.cashcontrol.modules.reporting;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 
 @Service
@@ -13,8 +13,14 @@ public class ThymeleafTemplate {
     private final SpringTemplateEngine pdfTemplateEngine;
 
     public String parseThymeleafTemplate() {
+
+        PdfVariables pdfVariables = PdfVariables.builder().
+                name("Reinhard").
+                vorname("Beat").
+                build();
+
         Context context = new Context();
-        context.setVariable("to", "Baeldung");
+        context.setVariable("data", pdfVariables);
 
         return pdfTemplateEngine.process("reporting/steuerrechnungs_daten", context);
     }
